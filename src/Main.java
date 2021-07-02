2e689267f3f5fa829913b57ccf36c876d37ca129
Bacenz <s3878466@rmit.edu.vn>
2021-07-02 19:41:01 +0700

    added swear method for no reason

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
-
+    public static class Me {
+        public void swear() {
+            System.out.println("Fuck");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("What the fuck is going on!");
+        Me ben = new Me();
+        ben.swear();
     }
 }
